716529ecd864dd4c4822907df8331869c270f0fd
Noam Rivlin <noam.rivlin15@gmail.com>
2024-02-01 18:26:23 +0200

    Add global styles and import Angular Material modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,53 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'calculator-app';
+  input: string = '';
+  readonly DIGITS = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0, '.'];
+  readonly OPERATORS = ['+', '-', '*', '/'];
+
+  handleDigitClick(digit: number | string) {
+    if (
+      digit === '.' &&
+      (!/\d$/gm.test(this.input) || /\d\.\d+$/gm.test(this.input))
+    ) {
+      return;
+    }
+    this.input += digit;
+  }
+
+  handleOperatorClick(operator: string) {
+    if (operator === '/' || operator === '*') {
+      this.input = this.input.replace(/\*|\/$/gm, operator);
+      if (/\d$/gm.test(this.input)) this.input += operator;
+    }
+
+    if (operator === '-' || operator === '+') {
+      if (!/\.$/gm.test(this.input)) this.input += operator;
+    }
+  }
+
+  handleClearEntry() {
+    if (isNaN(parseInt(this.input))) {
+      this.input = '';
+    } else {
+      this.input = this.input.slice(0, -1);
+    }
+  }
+
+  handleCalculate() {
+    try {
+      let sanitizedInput = this.input.replace(/(\d+(\.\d+)?)/g, (match) =>
+        parseFloat(match).toString()
+      );
+      sanitizedInput = sanitizedInput.replace('--', '+');
+      let calculatedResult = eval(sanitizedInput);
+      calculatedResult = parseFloat(calculatedResult.toFixed(4));
+      this.input = calculatedResult.toString();
+    } catch (error) {
+      this.input = 'Error';
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
+import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
